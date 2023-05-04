67aea3cabd13596f110160219a593069054187a4
Ihan Carlos Oliveira Botelho <ihancarlosihan@gmail.com>
2023-05-04 09:51:00 -0300

    Initial Commit

---
+++ OpenAIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenAIApp
+//
+//  Created by ihan carlos on 04/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
